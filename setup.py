f39868a09c20abc13541fec498108ec215b2739f
Dan Timachov <danyatimachov@gmail.com>
2020-09-13 13:25:59 +0300

    Migrating to package.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bgd-ghadd",
+    version="0.0.1",
+    author="Dan Timachov",
+    author_email="danyatimachov@gmail.com",
+    description="An utility to help you download everyday backgrounds matcking your taste",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ghadd/bg-downloader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Linux",
+    ],
+    python_requires='>=3.6',
+)
